796577a1e94e79622a6a86150730bc35f2f6b1af
Alexandre ANDRE <aandre8@gatech.edu>
2024-10-28 18:39:21 -0400

    :hammer:

---
+++ examples/ndt2/data_loader_generator.py
@@ -0,0 +1,82 @@
+from typing import List, Optional
+
+import torch
+from torch.utils.data import DataLoader
+from transforms import DropUnit, NDT2Tokenizer
+from utils import custom_sampling_intervals
+
+from brainsets.taxonomy import decoder_registry
+from torch_brain.data import Dataset, collate
+from torch_brain.data.sampler import (
+    RandomFixedWindowSampler,
+    SequentialFixedWindowSampler,
+)
+from torch_brain.transforms import Compose
+
+
+class DataLoaderGenerator:
+    def __init__(self, cfg, dataset_cfg, train_wrapper, is_ssl):
+        self.cfg = cfg
+        self.dataset_cfg = dataset_cfg
+        self.train_wrapper = train_wrapper
+        self.is_ssl = is_ssl
+
+        session_tokenizer = self.train_wrapper.patchifier.sess_emb.tokenizer
+
+        drop_sorted_unit = DropUnit(keyword="sorted")
+        tokenizer = NDT2Tokenizer(
+            ctx_time=cfg.ctx_time,
+            bin_time=cfg.bin_time,
+            patch_size=cfg.patch_size,
+            decoder_registry=decoder_registry,
+            mask_ratio=cfg.mask_ratio,
+            pad_val=cfg.pad_val,
+            sess_emb_space_idx=cfg.model.max_space_patches - 1,
+            sess_emb_time_idx=cfg.model.max_time_patches - 1,
+            session_tokenizer=session_tokenizer,
+            inc_behavior=not self.is_ssl,
+            inc_mask=self.is_ssl,
+        )
+        transforms = Compose([drop_sorted_unit, tokenizer])
+
+        self.dataset = Dataset(
+            root=cfg.data_root,
+            split="full",
+            include=self.dataset_cfg,
+            transform=transforms,
+        )
+
+        self.session_ids = self.dataset.get_session_ids()
+        res = custom_sampling_intervals(
+            self.dataset, cfg.ctx_time, train_ratio=0.8, seed=0
+        )
+        self.train_intervals = res[0]
+        self.eval_intervals = res[1]
+
+    def __call__(self, split: str) -> DataLoader:
+        cfg = self.cfg
+        if split == "train":
+            train_intervals = self.train_intervals
+        else:
+            train_intervals = self.eval_intervals
+        sampler = SequentialFixedWindowSampler(
+            interval_dict=train_intervals,
+            window_length=cfg.ctx_time,
+            step=cfg.ctx_time,
+            drop_short=True,
+        )
+
+        bs = cfg.batch_size_per_gpu if self.is_ssl else cfg.superv_batch_size_per_gpu
+        loader = DataLoader(
+            dataset=self.dataset,
+            batch_size=bs,
+            sampler=sampler,
+            collate_fn=collate,
+            num_workers=cfg.num_workers,
+            drop_last=split == "train",
+        )
+        return loader
+
+    def get_session_ids(self) -> List[str]:
+        assert self.session_ids is not None
+        return self.session_ids
---
+++ examples/ndt2/utils.py
@@ -0,0 +1,98 @@
+from collections import defaultdict
+from typing import List, Optional
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+import wandb
+from lightning.pytorch.callbacks import (
+    Callback,
+    LearningRateMonitor,
+    ModelCheckpoint,
+    ModelSummary,
+)
+from lightning.pytorch.loggers import WandbLogger
+
+from torch_brain.data import Dataset
+
+
+def balanced_accuracy_score(y_true, y_pred):
+    with torch.no_grad():
+        # Convert predictions to binary classes
+        y_pred_classes = (y_pred > 0.5).float()
+
+        # Confusion matrix elements
+        TP = (y_pred_classes * y_true).sum().item()
+        TN = ((1 - y_pred_classes) * (1 - y_true)).sum().item()
+        FP = ((1 - y_true) * y_pred_classes).sum().item()
+        FN = (y_true * (1 - y_pred_classes)).sum().item()
+
+        # Sensitivity for each class
+        sensitivity_pos = TP / (TP + FN)
+        sensitivity_neg = TN / (TN + FP)
+
+        # Balanced accuracy
+        balanced_acc = (sensitivity_pos + sensitivity_neg) / 2
+        return balanced_acc
+
+
+def set_callbacks(cfg) -> List[Callback]:
+    callbacks = [
+        ModelSummary(max_depth=3),
+        LearningRateMonitor(logging_interval="step"),
+    ]
+    if cfg.checkpoint.enable:
+        callbacks += [
+            ModelCheckpoint(
+                save_last=True,  # saves a checkpoint for the last epoch
+                every_n_train_steps=cfg.checkpoint.every_n_steps,
+                every_n_epochs=cfg.checkpoint.every_n_epochs,
+                save_top_k=-1 if cfg.checkpoint.save_all else 1,
+            )
+        ]
+    return callbacks
+
+
+def wandb_set_up(cfg, log) -> Optional[WandbLogger]:
+    if not cfg.wandb.enable:
+        return None
+    wandb_logger = WandbLogger(
+        entity=cfg.wandb.entity,
+        project=cfg.wandb.project,
+        name=cfg.wandb.run_name,
+        save_dir=cfg.log_dir,
+        log_model=False,
+    )
+    log.info(f"Using wandb logger: {wandb_logger.version}")
+
+    return wandb_logger
+
+
+def custom_sampling_intervals(dataset: Dataset, ctx_time=1.0, train_ratio=0.8, seed=0):
+    session_cache = {}
+    ses_keys = []
+    for ses_id in dataset.get_session_ids():
+        ses = dataset.get_session_data(ses_id)
+        session_cache[ses_id] = ses
+        nb_trials = int(ses.domain.end[-1] - ses.domain.start[0])
+        for i in range(nb_trials):
+            ses_keys.append(f"{ses_id}-{i}")
+
+    pl.seed_everything(seed)
+    np.random.shuffle(ses_keys)
+    tv_cut = int(train_ratio * len(ses_keys))
+    train_keys, val_keys = ses_keys[:tv_cut], ses_keys[tv_cut:]
+
+    def get_dict(keys, session_cache):
+        d = defaultdict(list)
+        for k in keys:
+            ses_id, trial = k.split("-")
+            ses = session_cache[ses_id]
+            ses_start = ses.domain.start[0]
+            offset = ctx_time * int(trial)
+            start = ses_start + offset
+            end = start + ctx_time
+            d[ses_id].append((start, end))
+        return dict(d)
+
+    return get_dict(train_keys), get_dict(val_keys)
