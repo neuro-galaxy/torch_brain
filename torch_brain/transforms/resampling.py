30dd437475c4bf5ea407077e95bc634d6c751751
Mehdi Azabou <mehdiazabou@gmail.com>
2025-04-16 14:41:59 -0400

    restructure args

--- torch_brain/transforms/resampling.py
+++ torch_brain/transforms/resampling.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Callable, List, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 import numpy as np
 from scipy.interpolate import interp1d
@@ -10,100 +10,137 @@ from temporaldata import Data, Interval, IrregularTimeSeries, RegularTimeSeries
 class Resampler:
     def __init__(
         self,
+        args: List[Dict[str, Any]],
+        *,
         target_sampling_rate: float,
-        target_keys: List[str],
-        shouldAlign: bool = True,
-        resample_fn: Optional[Callable] = None,
+        method: Optional[str] = "decimate",
     ):
-        self.target_sampling_rate = target_sampling_rate
-        self.target_keys = target_keys
-        if resample_fn is None:
-            self.resample_fn = self.dflt_resample_fn
-        else:
-            self.resample_fn = resample_fn
-        self.shouldAlign = shouldAlign
-
-        # To avoid aliasing, we add a buffer of 10 * sampling_rate / target_sampling_rate
-        # points on left/right side, which is equivalent to 10 / target_sampling_rate seconds
-        self._anti_aliasing_buffer = 10.0 / self.target_sampling_rate
-
-        self.needs_target_domain = True
-
-    def __call__(self, data: Data, target_domain: Interval):
-        if len(target_domain.start) != 1 or len(target_domain.end) != 1:
-            raise ValueError(
-                f"target_domain must be a 1D Interval, got {target_domain}"
-            )
-
-        start, end = target_domain.start[0], target_domain.end[0]
-
-        if self.shouldAlign:
-            offset = target_domain.start[0] % (1 / self.target_sampling_rate)
-            start = start - offset
-
-        if start < data.domain.start[0]:
-            raise ValueError(
-                f"target_domain.start {start} is outside the data domain {data.domain}"
-            )
-        if end > data.domain.end[-1]:
-            raise ValueError(
-                f"target_domain.end {end} is outside the data domain {data.domain}"
-            )
-
-        # Ensure the buffer_start and buffer_end are inside the data domain
-        buffer_start = max(start - self._anti_aliasing_buffer, data.domain.start[0])
-        buffer_end = min(end + self._anti_aliasing_buffer, data.domain.end[-1])
-
-        sliced_buffered_data = data.slice(buffer_start, buffer_end, reset_origin=False)
-
-        resampled_data = self.resample(
-            sliced_buffered_data, Interval(buffer_start, buffer_end)
-        )
-
-        return resampled_data.slice(start, end, reset_origin=False)
-
-    def resample(self, data: Data, target_domain: Interval):
-        r"""Resample the data.
+        r"""
+        args=[{"target_key": "data_key", "target_sampling_rate": ..., "original_sampling_rate": ..., "method": "decimate"}, ...]
 
         Args:
-            data (Data): The data to resample.
+            args (List[Dict[str, Any]]): The arguments for the resampling.
+            target_sampling_rate (float): The target sampling rate.
+            method (str): The method to use for resampling.
         """
-        for key in self.target_keys:
-            timeseries = data.get_nested_attribute(key)
+        self.args = args
+        for arg in self.args:
+            # Set default values if not provided in the argument
+            if "target_sampling_rate" not in arg:
+                arg["target_sampling_rate"] = target_sampling_rate
+
+            if "method" not in arg:
+                arg["method"] = method
+
+            if arg["method"] not in ["decimate"]:
+                raise ValueError(
+                    f"method {arg['method']} is not supported, only decimate is supported"
+                )
+
+            # To avoid aliasing, we add a buffer of 10 * sampling_rate / target_sampling_rate
+            # points on left/right side, which is equivalent to 10 / target_sampling_rate seconds
+            arg["anti_aliasing_buffer"] = 10.0 / arg["target_sampling_rate"]
+
+    def __call__(self, data: Data, target_domain: Interval):
+        if len(target_domain) != 1:
+            raise ValueError(
+                f"target_domain must contain a single interval, got {len(target_domain)}"
+            )
+
+        for arg in self.args:
+            target_key = arg["target_key"]
+            target_sampling_rate = arg["target_sampling_rate"]
+            anti_aliasing_buffer = arg["anti_aliasing_buffer"]
+            method = arg["method"]
+
+            timeseries = data.get_nested_attribute(target_key)
+            if not isinstance(timeseries, (RegularTimeSeries, IrregularTimeSeries)):
+                raise ValueError(f"target_key {target_key} is not a timeseries")
+
+            start, end = target_domain.start[0], target_domain.end[0]
+
+            # check that target_domain is included in data domain
+            _overlap = timeseries.domain & target_domain
+            if (
+                len(_overlap) != 1
+                or _overlap.start[0] != start
+                or _overlap.end[0] != end
+            ):
+                raise ValueError(
+                    f"target_domain {target_domain} is not included in timeseries domain {timeseries.domain}"
+                )
+
+            # add buffer to deal with anti-aliasing
+            target_domain_with_buffer = target_domain.dilate(anti_aliasing_buffer)
+            target_domain_with_buffer = target_domain_with_buffer & timeseries.domain
+
+            if len(target_domain_with_buffer) > 1:
+                idx = np.where(
+                    np.logical_and(
+                        start >= target_domain_with_buffer.start,
+                        end <= target_domain_with_buffer.end,
+                    )
+                )[0]
+                target_domain_with_buffer = Interval(
+                    target_domain_with_buffer.start[idx],
+                    target_domain_with_buffer.end[idx],
+                )
+
+            start_with_buffer = target_domain_with_buffer.start[0]
+            end_with_buffer = target_domain_with_buffer.end[0]
+            start_with_buffer = start_with_buffer + (start - start_with_buffer) % (
+                1.0 / self.target_sampling_rate
+            )
+
+            # slice data around the target domain
+            timeseries = timeseries.slice(
+                start_with_buffer, end_with_buffer, reset_origin=False
+            )
 
             if isinstance(timeseries, IrregularTimeSeries):
                 timeseries = irregular_to_regular_timeseries(
-                    timeseries, target_domain=target_domain
+                    timeseries,
+                    target_domain=target_domain,
+                    target_sampling_rate=arg.get("original_sampling_rate", None),
                 )
 
-            # downsample
-            downsample_factor = timeseries.sampling_rate / self.target_sampling_rate
-
-            if downsample_factor <= 1:
-                raise ValueError(f"Upsampling is not supported")
-
-            if not downsample_factor.is_integer():
-                raise ValueError(
-                    f"Downsample factor {downsample_factor} is not an integer"
+            # resample data
+            if method == "decimate":
+                timeseries_resampled = _decimate(
+                    timeseries,
+                    Interval(start_with_buffer, end_with_buffer),
+                    target_sampling_rate,
                 )
 
-            downsample_factor = int(downsample_factor)
-
-            # Create a new RegularTimeSeries with the resampled data
-            out = RegularTimeSeries(
-                sampling_rate=self.target_sampling_rate, domain=timeseries.domain
-            )
-            for att_key in timeseries.keys():
-                x_in = getattr(timeseries, att_key)
-                x_out = self.resample_fn(x_in, downsample_factor)
-                setattr(out, att_key, x_out)
-
-            setattr(data, key, out)
+            setattr(data, target_key, timeseries_resampled)
 
         return data
 
-    def dflt_resample_fn(self, x, downsample_factor):
-        return decimate(x, downsample_factor, axis=0, ftype="iir")
+
+def _decimate(timeseries: RegularTimeSeries, target_sampling_rate: float):
+    # compute the downsampling factor
+    downsample_factor = timeseries.sampling_rate / target_sampling_rate
+
+    if downsample_factor <= 1:
+        raise ValueError(f"Upsampling is not supported")
+
+    if not downsample_factor.is_integer():
+        raise ValueError(f"Downsample factor {downsample_factor} is not an integer")
+
+    downsample_factor = int(downsample_factor)
+
+    # create a new RegularTimeSeries with the resampled data
+    out = RegularTimeSeries(
+        sampling_rate=target_sampling_rate, domain=timeseries.domain
+    )
+
+    # resample each attribute
+    for att_key in timeseries.keys():
+        x_in = getattr(timeseries, att_key)
+        x_out = decimate(x_in, downsample_factor, axis=0, ftype="iir")
+        setattr(out, att_key, x_out)
+
+    return out
 
 
 def irregular_to_regular_timeseries(
@@ -111,6 +148,14 @@ def irregular_to_regular_timeseries(
     target_sampling_rate: float = None,
     target_domain: Interval = None,
 ):
+    r"""Resample an irregular time series to a regular time series.
+    This assumes that the irregular time series is almost sampled at a regular interval.
+
+    Args:
+        timeseries (IrregularTimeSeries): The irregular time series to resample.
+        target_sampling_rate (float): The target sampling rate.
+        target_domain (Interval): The target domain.
+    """
     dt = np.diff(timeseries.timestamps)
 
     if target_sampling_rate is None:
@@ -124,14 +169,13 @@ def irregular_to_regular_timeseries(
     if target_domain is None:
         domain = Interval(timeseries.timestamps[0], timeseries.timestamps[-1])
     else:
-        # Check that target_domain is a valid interval
         if not isinstance(target_domain, Interval):
             raise ValueError(
                 f"target_domain must be an Interval, got {type(target_domain)}"
             )
-        if len(target_domain.start) != 1 or len(target_domain.end) != 1:
+        if len(target_domain) != 1:
             raise ValueError(
-                f"target_domain must be a 1D Interval, got {target_domain}"
+                f"target_domain must contain a single interval, got {len(target_domain)}"
             )
 
         # TODO could be too restrictive when irregular has an auto domain
@@ -149,11 +193,7 @@ def irregular_to_regular_timeseries(
 
     out = RegularTimeSeries(sampling_rate=target_sampling_rate, domain=domain)
 
-    timestamps_regular = np.arange(domain.start[0], domain.end[0], dt_target)
-
     for attr_key in timeseries.keys():
-        if attr_key == "timestamps":
-            continue
         interpolator = interp1d(
             timeseries.timestamps,
             getattr(timeseries, attr_key),
@@ -161,7 +201,7 @@ def irregular_to_regular_timeseries(
             kind="linear",
             fill_value="extrapolate",
         )
-        interpolated_values = interpolator(timestamps_regular)
+        interpolated_values = interpolator(out.timestamps)
         setattr(out, attr_key, interpolated_values)
 
     return out
