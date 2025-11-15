118b3c7a442c0d9d4b53c695263085142bec39e5
Pingsheng-NeuroAI <pingsheng.neuroai@gmail.com>
2025-11-14 22:14:46 -0500

    fix typo

--- torch_brain/models/__init__.py
+++ torch_brain/models/__init__.py
@@ -1,4 +1,4 @@
 from .poyo import POYO, poyo_mp
 from .poyo_plus import POYOPlus
 from .capoyo import CaPOYO
-from .poyo_adap_context_pad import POYOAdapContextPad
+from .poyo_adap_win import POYOAdapContextPad
