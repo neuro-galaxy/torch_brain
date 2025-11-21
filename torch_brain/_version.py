d83830c542889c74e7da70275e9d235770f11cad
Milo Sobral <milosobral@gmail.com>
2025-11-21 17:12:37 -0500

    Committed new version file

--- torch_brain/_version.py
+++ torch_brain/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev327+g1ddde5692.d20251121'
-__version_tuple__ = version_tuple = (0, 1, 'dev327', 'g1ddde5692.d20251121')
+__version__ = version = "0.1.dev328+g01b8c7047"
+__version_tuple__ = version_tuple = (0, 1, "dev328", "g01b8c7047")
 
 __commit_id__ = commit_id = None
