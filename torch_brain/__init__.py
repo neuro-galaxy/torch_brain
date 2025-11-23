dd0e40721bffe0b62e3fad222a4cf5dcbd9fcad2
Vinam Arora <vinam@seas.upenn.edu>
2025-11-22 19:48:51 -0500

    handle cov

--- torch_brain/__init__.py
+++ torch_brain/__init__.py
@@ -11,5 +11,6 @@ from .registry import register_modality, get_modality_by_id, MODALITY_REGISTRY
 
 try:
     __version__ = version("pytorch-brain")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
+    # This can happen if someone is importing torch_brain without installing
     pass  # pragma: no cover
