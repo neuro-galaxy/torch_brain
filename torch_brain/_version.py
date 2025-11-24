e1339cbe047fb6eb43eb6ff0ded732d2910a1314
Milo Sobral <milosobral@gmail.com>
2025-11-24 10:20:47 -0500

    Fixed the version problem when uploading to testPyPi

--- torch_brain/_version.py
+++ torch_brain/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev328+g01b8c7047"
-__version_tuple__ = version_tuple = (0, 1, "dev328", "g01b8c7047")
+__version__ = version = '0.1.dev330'
+__version_tuple__ = version_tuple = (0, 1, 'dev330')
 
 __commit_id__ = commit_id = None
