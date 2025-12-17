acae5758166412d41421bc45a53cad6118dee446
Vinam Arora <vinam@seas.upenn.edu>
2025-12-17 02:38:55 -0500

    export PeiPandarinathNLB2021

--- torch_brain/datasets/__init__.py
+++ torch_brain/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .dataset import Dataset, SpikingDatasetMixin, MultiDataset
 
 from .PerichMillerPopulation2018 import PerichMillerPopulation2018
+from .PeiPandarinathNLB2021 import PeiPandarinathNLB2021
