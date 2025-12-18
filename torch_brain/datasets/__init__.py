3d5aa169b7c98f0d556dab03284f42eab3eff663
Vinam Arora <vinam@seas.upenn.edu>
2025-12-18 02:49:11 -0500

    remove datasets. they have been added to brainsets.datasets

--- torch_brain/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-from .PerichMillerPopulation2018 import PerichMillerPopulation2018
-from .PeiPandarinathNLB2021 import PeiPandarinathNLB2021
-from .FlintSlutzkyAccurate2012 import FlintSlutzkyAccurate2012
-from .ChurchlandShenoyNeural2012 import ChurchlandShenoyNeural2012
-from .OdohertySabesNonhuman2017 import OdohertySabesNonhuman2017
