13999f20e6a358ca2fbcc509917e493cef240fe5
Vinam Arora <vinam@seas.upenn.edu>
2025-12-21 00:56:05 -0500

    add mixin for CalciumImaging data

--- torch_brain/dataset/__init__.py
+++ torch_brain/dataset/__init__.py
@@ -1,3 +1,3 @@
 from .dataset import Dataset, DatasetIndex
 from .nested import NestedDataset, NestedSpikingDataset
-from .mixins import SpikingDatasetMixin
+from .mixins import SpikingDatasetMixin, CalciumImagingDatasetMixin
--- torch_brain/dataset/mixins.py
+++ torch_brain/dataset/mixins.py
@@ -30,3 +30,31 @@ class SpikingDatasetMixin:
         """Return a sorted list of all unit IDs across all recordings in the dataset."""
         ans = [self.get_recording(rid).units.id for rid in self.recording_ids]
         return np.sort(np.concatenate(ans)).tolist()
+
+
+class CalciumImagingDatasetMixin:
+    """
+    Mixin class for :class:`torch_brain.dataset.Dataset` subclasses containing calcium imaging data.
+
+    Provides:
+        - ``get_roi_ids()`` for retrieving IDs of all included ROIs.
+        - If the class attribute ``calcium_imaging_dataset_mixin_uniquify_roi_ids`` is set to ``True``,
+          ROI IDs will be made unique across recordings by prefixing each ROI ID with the
+          corresponding ``session.id``. This helps avoid collisions when combining data from
+          multiple sessions. (default: ``False``)
+    """
+
+    calcium_imaging_dataset_mixin_uniquify_roi_ids: bool = False
+
+    def get_recording_hook(self, data: Data):
+        if self.calcium_imaging_dataset_mixin_uniquify_roi_ids:
+            data.rois.id = np_string_prefix(
+                f"{data.session.id}/",
+                data.rois.id.astype(str),
+            )
+        super().get_recording_hook(data)
+
+    def get_roi_ids(self) -> list[str]:
+        """Return a sorted list of all ROI IDs across all recordings in the dataset."""
+        ans = [self.get_recording(rid).rois.id for rid in self.recording_ids]
+        return np.sort(np.concatenate(ans)).tolist()
