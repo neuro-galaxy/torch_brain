ae5d99047951dbfd387e910de24f437a151e24f5
Vinam Arora <vinam@seas.upenn.edu>
2025-12-20 23:35:53 -0500

    cleanup

--- examples/poyo/datasets/poyo_1.py
+++ examples/poyo/datasets/poyo_1.py
@@ -1,41 +1,46 @@
 import torchmetrics
+from temporaldata import Data
+
 from brainsets.datasets import (
     ChurchlandShenoyNeural2012,
     FlintSlutzkyAccurate2012,
     OdohertySabesNonhuman2017,
 )
-from temporaldata import Data
-
+from torch_brain.dataset import NestedSpikingDataset
 from datasets.poyo_mp import PoyoMPDataset
-from torch_brain.dataset.nested import NestedSpikingDataset
 
 
-class Poyo1Dataset(NestedSpikingDataset):
-    def __init__(self, root, transform=None, **kwargs):
-        ds_mp = PoyoMPDataset(root)
-        ds_flint = PoyoFlintDataset(root)
-        ds_odoherty = PoyoOdohertyDataset(root)
-        ds_churchland = PoyoChurchlandDataset(root)
-        super().__init__(
-            datasets={
-                "mp": ds_mp,
-                "flint": ds_flint,
-                "odoherty": ds_odoherty,
-                "churchland": ds_churchland,
-            },
-            transform=transform,
-        )
+def Poyo1Dataset(root, transform=None, **kwargs):
+    ds_mp = PoyoMPDataset(root)
+    ds_flint = PoyoFlintDataset(root)
+    ds_odoherty = PoyoOdohertyDataset(root)
+    ds_churchland = PoyoChurchlandDataset(root)
+
+    ds = NestedSpikingDataset(
+        datasets={
+            "mp": ds_mp,
+            "flint": ds_flint,
+            "odoherty": ds_odoherty,
+            "churchland": ds_churchland,
+        },
+        transform=transform,
+        **kwargs,
+    )
+    return ds
 
 
-class PoyoFlintDataset(FlintSlutzkyAccurate2012):
+class PoyoChurchlandDataset(ChurchlandShenoyNeural2012):
     READOUT_CONFIG = {
         "readout": {
             "readout_id": "cursor_velocity_2d",
-            # we will use the hand velocity for this dataset
-            "timestamp_key": "hand.timestamps",
-            "value_key": "hand.vel",
             "normalize_mean": 0.0,
-            "normalize_std": 0.4,
+            "normalize_std": 800.0,
+            "weights": {
+                "movement_phases.hold_period": 0.1,
+                "movement_phases.reach_period": 5.0,
+                "movement_phases.return_period": 1.0,
+                "cursor_outlier_segments": 1.0,
+            },
             "metrics": [{"metric": torchmetrics.R2Score()}],
         }
     }
@@ -60,18 +65,15 @@ class PoyoOdohertyDataset(OdohertySabesNonhuman2017):
         return super().get_recording_hook(data)
 
 
-class PoyoChurchlandDataset(ChurchlandShenoyNeural2012):
+class PoyoFlintDataset(FlintSlutzkyAccurate2012):
     READOUT_CONFIG = {
         "readout": {
             "readout_id": "cursor_velocity_2d",
+            # we will use the hand velocity for this dataset
+            "timestamp_key": "hand.timestamps",
+            "value_key": "hand.vel",
             "normalize_mean": 0.0,
-            "normalize_std": 800.0,
-            "weights": {
-                "movement_phases.hold_period": 0.1,
-                "movement_phases.reach_period": 5.0,
-                "movement_phases.return_period": 1.0,
-                "cursor_outlier_segments": 1.0,
-            },
+            "normalize_std": 0.4,
             "metrics": [{"metric": torchmetrics.R2Score()}],
         }
     }
