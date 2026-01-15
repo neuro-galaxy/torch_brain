130115357a4c23d38b9ba5527de402e9a38a5cd9
AlexandreAndr <alexandreandre02018@gmail.com>
2026-01-15 14:02:35 -0500

    test

--- torch_brain/utils/binning.py
+++ torch_brain/utils/binning.py
@@ -51,9 +51,11 @@ def bin_spikes(
 
     num_bins = round((end - start) / bin_size)
 
+    print(spikes.timestamps)
+
     rate = 1 / bin_size  # avoid precision issues
     binned_spikes = np.zeros((num_units, num_bins), dtype=dtype)
-    bin_index = np.floor((spikes.timestamps) * rate).astype(int)
+    bin_index = np.floor(spikes.timestamps * rate).astype(int)
     np.add.at(binned_spikes, (spikes.unit_index, bin_index), 1)
 
     return binned_spikes
