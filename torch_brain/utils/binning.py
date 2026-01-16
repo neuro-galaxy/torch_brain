d4e8d6da8a7e24275a711e028381cecc24c07bc9
AlexandreAndr <alexandreandre02018@gmail.com>
2026-01-16 15:53:31 -0800

    update changelog

--- torch_brain/utils/binning.py
+++ torch_brain/utils/binning.py
@@ -9,9 +9,9 @@ def bin_spikes(
     num_units: int,
     bin_size: float,
     max_spikes: Optional[int] = None,
-    right: bool = True,
-    eps: float = 1e-3,
-    dtype: np.dtype = np.float32,
+    right: Optional[bool] = True,
+    eps: Optional[float] = 1e-3,
+    dtype: Optional[np.dtype] = np.float32,
 ) -> np.ndarray:
     r"""Bins spikes into time bins of size `bin_size`. If the total time spanned by
     the spikes is not a multiple of `bin_size`, the spikes are truncated to the nearest
