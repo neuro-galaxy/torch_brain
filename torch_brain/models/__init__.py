b30799ccbc0d765e97b8d861394e884ca37898da
divyansha1115 <divyansha1115@gmail.com>
2026-04-19 19:24:57 -0400

    added poyo mp

--- torch_brain/models/__init__.py
+++ torch_brain/models/__init__.py
@@ -2,11 +2,7 @@ from .poyo import POYO, poyo_mp
 from .poyo_plus import POYOPlus
 from .capoyo import CaPOYO
 
-__all__ = [
-    "POYO",
-    "POYOPlus",
-    "CaPOYO",
-]
+__all__ = ["POYO", "POYOPlus", "CaPOYO", "poyo_mp"]
 
 # see docs/source/api_reference.py
 __api_ref__ = {
