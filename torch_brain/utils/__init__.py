37ea365dc7a68f52eae139a7f762cafe6b9df62f
Vinam Arora <vinam@seas.upenn.edu>
2026-05-09 04:38:22 -0400

    cleanup!: Rm `get_sinusoidal_encoding` (legacy code) (#200)
    Removes `get_sinusoidal_encoding` util function.

--- torch_brain/utils/__init__.py
+++ torch_brain/utils/__init__.py
@@ -1,6 +1,5 @@
 from .seed_everything import seed_everything
 from .tokenizers import create_linspace_latent_tokens, create_start_end_unit_tokens
-from .sinusoidal_encoding import get_sinusoidal_encoding
 from .weights import resolve_weights_based_on_interval_membership, isin_interval
 from .readout import prepare_for_readout
 from .misc import np_string_prefix
@@ -12,7 +11,6 @@ __all__ = [
     "seed_everything",
     "create_linspace_latent_tokens",
     "create_start_end_unit_tokens",
-    "get_sinusoidal_encoding",
     "resolve_weights_based_on_interval_membership",
     "isin_interval",
     "prepare_for_readout",
