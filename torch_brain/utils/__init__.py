4c763ccc712cb19db0daee7f884443bb249dbe87
Alexandre André <93080557+AlexandreAndr@users.noreply.github.com>
2026-05-19 23:54:59 -0400

    cleanup!: Remove `utils.readout` (#218)

--- torch_brain/utils/__init__.py
+++ torch_brain/utils/__init__.py
@@ -1,7 +1,6 @@
 from .seed_everything import seed_everything
 from .tokenizers import create_linspace_latent_tokens, create_start_end_unit_tokens
 from .weights import resolve_weights_based_on_interval_membership, isin_interval
-from .readout import prepare_for_readout
 from .misc import np_string_prefix
 from .binning import bin_spikes
 from .stitcher import stitch
@@ -13,7 +12,6 @@ __all__ = [
     "create_start_end_unit_tokens",
     "resolve_weights_based_on_interval_membership",
     "isin_interval",
-    "prepare_for_readout",
     "np_string_prefix",
     "bin_spikes",
 ]
