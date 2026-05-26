92089ca2bb09e0c8d44e30f53b758eb20e247184
Alexandre André <93080557+AlexandreAndr@users.noreply.github.com>
2026-05-26 04:14:15 -0400

    cleanup!: rm seed_everything (#227)
    `torch_brain.utils.seed_everything` moved out of the public API; a
    reference implementation is still available in `examples/poyo/utils.py`
    and `examples/poyo_plus/utils.py`.
    Calling it now raises an `ImportError` pointing to the POYO example.

--- torch_brain/utils/__init__.py
+++ torch_brain/utils/__init__.py
@@ -1,13 +1,19 @@
-from .seed_everything import seed_everything
 from .tokenizers import create_linspace_latent_tokens, create_start_end_unit_tokens
 from .weights import resolve_weights_based_on_interval_membership, isin_interval
 from .misc import np_string_prefix
 from .binning import bin_spikes
 from .stitcher import stitch
 
+
+def seed_everything(*args, **kwargs):
+    raise ImportError(
+        "`seed_everything` has been removed from `torch_brain.utils`. "
+        "You can find a reference implementation in `examples/poyo/utils.py`"
+    )
+
+
 __all__ = [
     "stitch",
-    "seed_everything",
     "create_linspace_latent_tokens",
     "create_start_end_unit_tokens",
     "resolve_weights_based_on_interval_membership",
