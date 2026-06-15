acd742b63c91377519f19e3eecb9bb04a8d5e429
Vinam Arora <vinam@seas.upenn.edu>
2026-06-15 03:02:10 -0400

    torch_brain.datasets

--- torch_brain/datasets/__init__.py
+++ torch_brain/datasets/__init__.py
@@ -1,36 +1,5 @@
-"""Base classes for creating PyTorch datasets
-
-Module Overview
----------------
-
-This module contains base classes to ease creation of PyTorch datasets for your data.
-
-- The :class:`Dataset` class is inherited by all datasets. These handle opening and accessing single datasets.
-- The :class:`NestedDataset` class is for opening and accessing multiple datasets through a unified interface.
-- :ref:`Mixin <datasets_ref-mixins>` classes are provided to add modality-specific functionalities to the Dataset classes.
-
-
-Dataset
-^^^^^^^
-
-torch_brain's :class:`Dataset` class (and its sub-classes) allow you to sample *time-slices* of your data.
-This is a major deviation from the standard :class:`torch.utils.data.Dataset`, which is indexed by integers.
-To achieve arbitrary time-slice based access, our Dataset class is indexed by three things:
-
-1. The recording id from which you want the slice,
-2. Start time of the slice, and
-3. End time of the slice
-
-These are put into a :class:`DatasetIndex` object, which is then used to index the :class:`Dataset`.
-Since different machine learning applications require different ways of sampling, we provide a collection of
-:ref:`samplers <samplers_ref>` which are responsible for creating these :class:`DatasetIndex` objects.
-
-NestedDataset
-^^^^^^^^^^^^^
-
-The :class:`Dataset` class is designed to operate on a single dataset. However, many modern ML methods perform
-training over multiple datasets. For this, we provide :class:`NestedDataset` that allows users to open and index through
-multiple datasets.
+"""Module containing base classes for creating PyTorch-compatible Datasets,
+as well as a library of pre-written Dataset classes for a number of brainsets.
 """
 
 __all__ = [
@@ -57,12 +26,6 @@ __all__ = [
     "OpenNeuroSplitType",
 ]
 
-# see docs/source/api_reference.py
-__api_ref__ = {
-    "description": None,
-    "sections": [],
-}
-
 from .AllenVisualCodingOphys2016 import AllenVisualCodingOphys2016
 from .ChurchlandShenoyNeural2012 import ChurchlandShenoyNeural2012
 from .dataset import Dataset, DatasetIndex
@@ -84,24 +47,55 @@ from .PerichMillerPopulation2018 import PerichMillerPopulation2018
 from .ShiraziHBNR1DS005505 import ShiraziHBNR1DS005505
 from .VollanMoserAlternating2025 import VollanMoserAlternating2025
 
+_base_classes_doc = """
+Base classes to ease creation of PyTorch-compatible Datasets for your data.
+
+- The :class:`Dataset` class is inherited by all datasets. These handle opening
+  and accessing single datasets.
+- The :class:`NestedDataset` class is for opening and accessing multiple
+  datasets through a unified interface.
+- :ref:`Mixin <datasets_ref-mixins>` classes are provided to add
+  modality-specific functionalities to the Dataset classes.
+
+
+**Dataset:** torch_brain's :class:`Dataset` class (and its sub-classes) allow
+you to sample *time-slices* of your data. This is a major deviation from the
+standard :class:`torch.utils.data.Dataset`, which is indexed by integers. To
+achieve arbitrary time-slice based access, our Dataset class is indexed by
+three things:
+
+1. The recording id from which you want the slice,
+2. Start time of the slice, and
+3. End time of the slice
+
+These are put into a :class:`DatasetIndex` object, which is then used to index
+the :class:`Dataset`. Since different machine learning applications require
+different ways of sampling, we provide a collection of :ref:`samplers
+<samplers_ref>` which are responsible for creating these :class:`DatasetIndex`
+objects.
+
+See
+:doc:`NLB Maze minimal example </generated/notebooks/nlb_maze_minimal_example>`
+for an example of how to create your own Dataset subclasses.
+
+**NestedDataset:** The :class:`Dataset` class is designed to operate on a
+single dataset. However, many modern ML methods perform training over multiple
+datasets. For this, we provide :class:`NestedDataset` that allows users to open
+and index through multiple datasets.
+"""
+
 # Drives the generated API reference; see docs/source/api_reference.py.
 __api_ref__ = {
     "description": None,
     "sections": [
         {
-            "title": "Base Classes",
-            "description": None,
+            "title": "Base Classes & Mixins",
+            "description": _base_classes_doc,
             "autosummary": [
                 "Dataset",
                 "DatasetIndex",
                 "NestedDataset",
                 "OpenNeuroDataset",
-            ],
-        },
-        {
-            "title": "Mixins",
-            "description": None,
-            "autosummary": [
                 "SpikingDatasetMixin",
                 "CalciumImagingDatasetMixin",
                 "MultiChannelDatasetMixin",
@@ -151,3 +145,4 @@ __api_ref__ = {
         },
     ],
 }
+
